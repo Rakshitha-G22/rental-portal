fb3c18b57759a9e427e38e501ea766d37fcc95fd
Rakshitha G <rakshithag227@gmail.com>
2026-03-01 23:55:44 +0530

    Fix railway deployment port and config

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 from app import create_app, db
+import os
 
 app = create_app()
 
+
+
+port = int(os.environ.get("PORT", 8080))
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port)
