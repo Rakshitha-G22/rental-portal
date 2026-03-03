a67f725443636effe9ddaa3d3f061d9b6fe5c0f1
Rakshitha G <rakshithag227@gmail.com>
2026-03-03 15:02:43 +0530

    Fix auth system

--- frontend/user-app/src/app/auth/auth.component.ts
+++ frontend/user-app/src/app/auth/auth.component.ts
@@ -1,78 +1,68 @@
+import { environment } from '../../environments/environment';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { environment } from '../../environments/environment';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth.component.html',
-  standalone: true
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,        
+    HttpClientModule
+  ],
+  templateUrl: './auth.component.html'
 })
 export class AuthComponent {
 
   isLogin = true;
 
-  name = '';
-  email = '';
-  password = '';
+  formData = {
+    name: '',
+    email: '',
+    password: ''
+  };
 
-  errorMsg = '';
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) {}
+  
 
   toggleMode() {
     this.isLogin = !this.isLogin;
-    this.errorMsg = '';
   }
 
   submit() {
 
     if (this.isLogin) {
-
       // LOGIN
-      this.http.post<any>(
-        `${environment.apiUrl}/auth/login`,
-        {
-          email: this.email,
-          password: this.password
+      
+      this.http.post<any>('http://localhost:5000/api/auth/login', {
+        email: this.formData.email,
+        password: this.formData.password
+      }).subscribe(res => {
+
+        localStorage.setItem('access_token', res.access_token);
+        localStorage.setItem('role', res.role);
+        localStorage.setItem('name', res.name);
+
+        if (res.role === 'admin') {
+          this.router.navigate(['/admin']);
+        } else {
+          this.router.navigate(['/flats']);
         }
-      ).subscribe({
-        next: (res) => {
 
-          localStorage.setItem("token", res.token);
-          localStorage.setItem("name", res.name);
 
-          this.router.navigate(['/dashboard']);
-
-        },
-        error: () => {
-          this.errorMsg = "Login failed";
-        }
       });
-
     } else {
-
       // REGISTER
-      this.http.post<any>(
-        `${environment.apiUrl}/auth/register`,
-        {
-          name: this.name,
-          email: this.email,
-          password: this.password
-        }
-      ).subscribe({
-        next: () => {
-          alert("Registration successful");
+     this.http.post(`${environment.apiUrl}/api/auth/register`, this.formData)
+        .subscribe(() => {
+          alert("Registration successful! Please login.");
           this.isLogin = true;
-        },
-        error: () => {
-          this.errorMsg = "Registration failed";
-        }
-      });
-
+        });
     }
   }
-}
+} 
