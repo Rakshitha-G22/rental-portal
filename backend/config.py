c8b3612842d6c72d9dbe2241bd52059c0bf420cb
Rakshitha G <rakshithag227@gmail.com>
2026-03-10 18:52:27 +0530

    database config update

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,10 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        'postgresql://postgres:Rakshu%40123@localhost:5432/rental-portal'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:Rakshu%40123@localhost:5432/rental-portal"
     )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'super-secret'
+    JWT_SECRET_KEY = "super-secret"
