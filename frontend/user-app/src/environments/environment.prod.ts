e8f154e36696195f664290d24cb81b1c276af112
Rakshitha G <rakshithag227@gmail.com>
2026-03-11 19:44:20 +0530

    Updated API URL for production

--- frontend/user-app/src/environments/environment.prod.ts
+++ frontend/user-app/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
-// environment.prod.ts
 export const environment = {
   production: true,
-  // This tells the browser: "Use whatever protocol the main page is using"
-  apiUrl: '//rental-portal-production.up.railway.app/api' 
+  apiUrl: 'https://rental-portal-backend-hy1p.onrender.com' 
 };
